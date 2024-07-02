5994dcf28db79c7116e1824b30b3b3517686b193
Ehsan Jahangirzadeh Soure <ehsan.soure@stackadapt.com>
2024-07-02 11:47:53 -0400

    Update renovate

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,7 +1,5 @@
 module.exports = {
   "branchPrefix": "test-renovate/",
-  "username": "renoappjso[bot]",
-  "gitAuthor": "RenoAppJso Bot <123456+renoappjso[bot]@users.noreply.github.enterprise.com>",
   "onboarding": true,
   "platform": "github",
   "forkProcessing": "enabled",
